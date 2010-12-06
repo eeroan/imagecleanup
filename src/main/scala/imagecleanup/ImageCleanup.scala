017d2de34d5187eddcd581df1406265e235ee1c8
Eero Anttila <eea@iki.fi>
2010-12-06 14:42:09 +0200

    move paths to system properties

--- src/main/scala/imagecleanup/ImageCleanup.scala
+++ src/main/scala/imagecleanup/ImageCleanup.scala
@@ -6,32 +6,15 @@ import javax.imageio.ImageIO
 object ImageCleanup {
   val ignoredFiles = List("Thumbs.db", ".DS_Store", ".localized", ".picasa.ini")
   val ignoredDirs = List(".picasaoriginals")
-  val extensions = List("jpg","png","jpeg","tiff","gif","bmp")
-  val temp = new File("/Users/eea/temp")
+  val extensions = List("jpg", "png", "jpeg", "tiff", "gif", "bmp")
+  val temp = new File(System.getProperty("tempDir"))
+
   def main(args: Array[String]) {
-    val root = new File("/Users/eea/Pictures")
+    val root = new File(System.getProperty("rootDir"))
     //checkFolder(root)
-    deleteCorruptedImages(root)
+    moveCorruptedImagesToTemp(root)
   }
 
-  def isOk(img: File) = try {
-    ImageIO.read(img).getWidth > 0
-  } catch {
-    case e: Exception => false
-  }
-  def moveIfNotOk(img: File) {
-    if(!isOk(img)) img.renameTo(new File(temp, img.getName))
-  }
-
-  def deleteCorruptedImages(dir: File) {
-    println(dir.getAbsolutePath)
-    val (allFiles, allDirs) = filesAndDirs(dir)
-    allFiles.filter(f => extensions.contains(f.getName.split('.').last.toLowerCase)).foreach(moveIfNotOk)
-    filteredDirs(allDirs).foreach(deleteCorruptedImages)
-  }
-
-  def filesAndDirs(dir: File) = dir.listFiles.toList.partition(_.isFile)
-  def filteredDirs(files: List[File]) = files.filter(f => !ignoredDirs.contains(f.getName))
   def checkFolder(dir: File) {
     val (allFiles, allDirs) = filesAndDirs(dir)
     val files = allFiles.map(_.getName).filter(!ignoredFiles.contains(_))
@@ -43,6 +26,28 @@ object ImageCleanup {
     }
     allDirs.foreach(checkFolder)
   }
+
   def pretty(files: List[String]) = files.mkString("\n")
+
+  def moveCorruptedImagesToTemp(dir: File) {
+    println(dir.getAbsolutePath)
+    val (allFiles, allDirs) = filesAndDirs(dir)
+    allFiles.filter(f => extensions.contains(f.getName.split('.').last.toLowerCase)).foreach(moveIfNotOk)
+    filteredDirs(allDirs).foreach(moveCorruptedImagesToTemp)
+  }
+
+  def moveIfNotOk(img: File) {
+    if (!isOk(img)) img.renameTo(new File(temp, img.getName))
+  }
+
+  def filesAndDirs(dir: File) = dir.listFiles.toList.partition(_.isFile)
+
+  def isOk(img: File) = try {
+    ImageIO.read(img).getWidth > 0
+  } catch {
+    case e: Exception => false
+  }
+
+  def filteredDirs(files: List[File]) = files.filter(f => !ignoredDirs.contains(f.getName))
 }
 
