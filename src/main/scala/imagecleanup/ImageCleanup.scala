ba70e6d0dbeac85945698081cae07d77e5b930ba
Eero Anttila <eea@iki.fi>
2010-12-06 23:35:25 +0200

    rename files with hash

--- src/main/scala/imagecleanup/ImageCleanup.scala
+++ src/main/scala/imagecleanup/ImageCleanup.scala
@@ -2,17 +2,40 @@ package imagecleanup
 
 import java.io.{FileFilter, File}
 import javax.imageio.ImageIO
+import java.lang.String
 
 object ImageCleanup {
   val ignoredFiles = List("Thumbs.db", ".DS_Store", ".localized", ".picasa.ini")
   val ignoredDirs = List(".picasaoriginals")
-  val extensions = List("jpg", "png", "jpeg", "tiff", "gif", "bmp")
+  val extensions = List("jpg", "png", "jpeg", "tiff", "gif", "bmp","nef")
   val temp = new File(System.getProperty("tempDir"))
 
   def main(args: Array[String]) {
     val root = new File(System.getProperty("rootDir"))
     //checkFolder(root)
-    moveCorruptedImagesToTemp(root)
+    //moveCorruptedImagesToTemp(root)
+    renameFilesWithNumberSign(root);
+  }
+
+  def renameFilesWithNumberSign(root: File) {
+    forEachFolder(root, file => {
+      val name: String = file.getName
+      if (name.startsWith("#")) {
+        val start: String = name.substring(0, 3)
+        val newName = start match {
+          case "#SC" => name.replace("#", "D")
+          case "#sc" => name.replace("#", "d")
+          case "#MG" => name.replace("#", "I")
+          case "#mg" => name.replace("#", "i")
+          case "#MA" => name.replace("#", "I")
+          case "#ma" => name.replace("#", "i")
+          case _ => System.err.println(name); name
+        }
+        val newFile = new File(file.getParent, newName)
+        println(file.getAbsolutePath+" -> "+newFile.getAbsolutePath)
+        file.renameTo(newFile)
+      }
+    })
   }
 
   def checkFolder(dir: File) {
@@ -30,10 +53,14 @@ object ImageCleanup {
   def pretty(files: List[String]) = files.mkString("\n")
 
   def moveCorruptedImagesToTemp(dir: File) {
-    println(dir.getAbsolutePath)
+    forEachFolder(dir, file => moveIfNotOk(file))
+  }
+
+  def forEachFolder(dir: File, action: (File) => Unit) {
     val (allFiles, allDirs) = filesAndDirs(dir)
-    allFiles.filter(f => extensions.contains(f.getName.split('.').last.toLowerCase)).foreach(moveIfNotOk)
-    filteredDirs(allDirs).foreach(moveCorruptedImagesToTemp)
+    //println(dir.getAbsolutePath)
+    allFiles.filter(f => extensions.contains(f.getName.split('.').last.toLowerCase)).foreach(action)
+    filteredDirs(allDirs).foreach(forEachFolder(_, action))
   }
 
   def moveIfNotOk(img: File) {
